0a6d4352bb438d8ae8d0bb4d13cae3c14a5dd68e
François Rousseu <francois.rousseu@usherbrooke.ca>
2026-01-06 12:29:24 -0500

    added extra vars to species in the right place

--- scripts/jobs/niches_climatiques.r
+++ scripts/jobs/niches_climatiques.r
@@ -6,17 +6,17 @@ species <- c("Pseudacris triseriata", "Hemidactylium scutatum", "Gyrinophilus po
 species_vars <- list(
 `Pseudacris triseriata` = c("wetland", "marais", "marecage", "geomflat"), 
 `Hemidactylium scutatum` = c("tourbiere", "marais", "organique"), 
-`Gyrinophilus porphyriticus` = c("elevation", "ruggedness", "forest", "geomflat", "twi"), 
-`Desmognathus ochrophaeus` = c("elevation", "ruggedness", "forest", "geomflat", "twi"), 
-`Emydoidea blandingii` = c("wetland", "marais", "marecage", "geomflat", "water", "eau_peu_profonde"),
-`Glyptemys insculpta` = c("wetland", "marais", "marecage", "geomflat", "water", "eau_peu_profonde", "sand"),
-`Nerodia sipedon` = c("wetland", "marais", "marecage", "geomflat", "water", "eau_peu_profonde"),
+`Gyrinophilus porphyriticus` = c("elevation", "ruggedness", "forest", "geomflat", "twi", "distance_to_streams"), 
+`Desmognathus ochrophaeus` = c("elevation", "ruggedness", "forest", "geomflat", "twi", "distance_to_streams"), 
+`Emydoidea blandingii` = c("wetland", "marais", "marecage", "geomflat", "water", "eau_peu_profonde", "distance_to_streams", "distance_to_rivers"),
+`Glyptemys insculpta` = c("wetland", "marais", "marecage", "geomflat", "water", "eau_peu_profonde", "sand", "distance_to_streams", "distance_to_rivers"),
+`Nerodia sipedon` = c("wetland", "marais", "marecage", "geomflat", "water", "eau_peu_profonde", "distance_to_openwater"),
 `Lampropeltis triangulum` = c("wetland", "marais", "marecage", "geomflat", "water", "eau_peu_profonde"), 
-`Aquila chrysaetos` = c("elevation", "ruggedness"), 
+`Aquila chrysaetos` = c("elevation", "ruggedness", "roche"), 
 `Catharus bicknelli` = c("elevation", "ruggedness"), 
 `Setophaga cerulea` = c("forest", "ph", "silt", "nitrogen"), 
-`Coturnicops noveboracensis` = c("flat", "marais", "wetland", "prairie_humide"), 
-`Ixobrychus exilis` = c("geomflat", "wetland", "marais", "eau_peu_profonde"), 
+`Coturnicops noveboracensis` = c("geomflat", "marais", "wetland", "prairie_humide", "glaciomarin", "organique"), 
+`Ixobrychus exilis` = c("geomflat", "wetland", "marais", "eau_peu_profonde", "glaciomarin", "glaciolacustre", "alluvion"), 
 `Glaucomys volans` = c("forest")
 )
 
@@ -48,8 +48,6 @@ breeding_periods <- list(
 )
 
 run_model <- 1:9 
-
-
 
 info <- duckdbfs::open_dataset("data/atlas_2025-03-17.parquet", tblname = "atlas") |>
   filter(valid_scientific_name %in% species) |>
