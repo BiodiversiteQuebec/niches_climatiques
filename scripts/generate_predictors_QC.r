2ac4fb1a1323cc3a53a22dd21eb56cdfe6623043
frousseu <francois.rousseu@usherbrooke.ca>
2026-01-13 22:13:53 +0000

    gdalwarp and avoid overviews when aggregating to lower res

--- scripts/generate_predictors_QC.r
+++ scripts/generate_predictors_QC.r
@@ -399,7 +399,8 @@ cl <- makeCluster(10)
 registerDoParallel(cl)
 getDoParWorkers()
 foreach(i = 1:nrow(variables[1:nrow(variables), ])) %dopar% {
-cmd <- sprintf('gdal_translate -of COG -r average -tr 500 500 -co COMPRESS=DEFLATE %s/%s.tif %s/%s_lowres.tif', tmpath, variables$name[i], tmpath, variables$name[i])
+#cmd <- sprintf('gdal_translate -of COG -r average -tr 500 500 -co COMPRESS=DEFLATE %s/%s.tif %s/%s_lowres.tif', tmpath, variables$name[i], tmpath, variables$name[i])
+cmd <- sprintf('gdalwarp -of COG -r average -tr 500 500 -srcnodata -9999 -dstnodata -9999 -ovr NONE -co COMPRESS=DEFLATE %s/%s.tif %s/%s_lowres.tif', tmpath, variables$name[i], tmpath, variables$name[i]) # do not use overview in resampling
 system(cmd)
 }
 
