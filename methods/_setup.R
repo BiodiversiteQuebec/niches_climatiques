1643a9ea32ba1260ae064b6856e1fd7d683c89c5
frousseu <francois.rousseu@usherbrooke.ca>
2026-03-25 16:23:47 -0400

    methods

--- methods/_setup.R
+++ methods/_setup.R
@@ -6,3 +6,19 @@ library(kableExtra)
 library(scam)
 library(gt)
 library(rstac)
+library(flextable)
+
+
+spnames <- list(list("Pseudacris triseriata", "Rainette faux-grillon de l'Ouest", "Rainettes"), list("Hemidactylium scutatum", 
+    "Salamandre à quatre orteils", "Salamandres"), list("Gyrinophilus porphyriticus", "Salamandre pourpre", "Salamandres"), list("Desmognathus ochrophaeus", 
+    "Salamandre sombre des montagnes", "Salamandres"), list("Emydoidea blandingii", "Tortue mouchetée", "Tortues"), list("Glyptemys insculpta", 
+    "Tortue des bois", "Tortues"), list("Nerodia sipedon", "Couleuvre d'eau", "Couleuvres"), list("Lampropeltis triangulum", 
+    "Couleuvre tachetée", "Couleuvres"), list("Aquila chrysaetos", "Aigle royal", "Oiseaux"), list("Catharus bicknelli", 
+    "Grive de Bicknell", "Oiseaux"), list("Setophaga cerulea", "Paruline azurée", "Oiseaux"), list("Coturnicops noveboracensis", 
+    "Râle jaune", "Oiseaux"), list("Ixobrychus exilis", "Petit Blongios", "Oiseaux"), list("Glaucomys volans", 
+    "Petit Polatouche", "Mammifères"))
+
+splatin <- sapply(spnames, "[[", 1)
+spfr <- sapply(spnames, "[[", 2)
+spgroup <- sapply(spnames, "[[", 3) |>
+  factor(levels = c("Rainettes", "Salamandres", "Couleuvres", "Tortues", "Oiseaux", "Mammifères"))
